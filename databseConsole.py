6b9486a2b3bf7c8a055b0c897dca68e229267c67
ip2176 <ip2176@g.rit.edu>
2017-12-17 23:03:30 -0500

    Revert "Change the game to use non-blocking threads"
    This reverts commit 2a70e964d1cac328858dd6df41c4adf37ddc8e47.

--- databseConsole.py
+++ databseConsole.py
@@ -4,85 +4,8 @@ import operator
 from PyQt5.QtGui import QImage, QPalette, QBrush, QFont
 from PyQt5.QtWidgets import (QWidget, QLabel, 
      QLineEdit, QApplication, QPushButton)
-from PyQt5.QtCore import QCoreApplication, pyqtSlot, QSize, QTimer, QRunnable, QThreadPool
+from PyQt5.QtCore import QCoreApplication, pyqtSlot, QSize, QTimer
 from time import sleep
-
-
-class WaitLoop(QRunnable):
-
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-        self.is_running = True
-
-    def stop(self):
-        self.is_running = False
-
-    @pyqtSlot()
-    def run(self):
-        code_text = None
-        success_message_text = ''
-        message_text = ''
-        num_guessed = 0
-
-        self.parent.input_text = self.parent.qline_edit.text()
-        self.parent.qline_edit.clear()
-        self.parent.update_label(self.parent.code_label, '')
-
-        for i in self.parent.spinner.calculate_rotations():
-            self.parent.update_label(self.parent.message_label, self.parent.check_message(self.parent.input_text))
-            sleep(self.parent.spinner.speed)
-        self.parent.update_label(self.parent.message_label, 'Done')
-        self.parent.spinner.reset()
-        sleep(0.5)
-
-        for key, value in self.parent.secret_keys.items():
-            if self.parent.input_text.upper() == key:
-                message_text = 'Key entered'
-                code_text = 'Key accepted'
-                self.parent.secret_keys[key] = True
-                break
-
-        if code_text is None:
-            message_text = self.parent.not_found_message()
-            code_text = ''
-
-        for key, value in self.parent.secret_keys.items():
-            if value:
-                num_guessed += 1
-
-        if num_guessed == len(self.parent.secret_keys.keys()):
-            message_text = 'Top Secret access granted. Welcome Agent 69-88'
-            success_message_text = 'Document Password:'
-                                
-            code_text = self.parent.secret_password
-            self.parent.game_end_keys_entered = True
-
-        # Game over
-        if self.parent.tries_remaining == 0 and code_text == '':
-            message_text = 'Database shutdown, system intrusion detected'
-            self.parent.game_end_fail = True
-
-        # When we click the button make sure we update the messages after processing
-        screen_updated = True
-        if self.is_running:
-            self.parent.update_label(self.parent.message_label, message_text)
-            self.parent.update_label(self.parent.success_message_label, success_message_text)
-            self.parent.update_label(self.parent.code_label, code_text)
-        else:
-            screen_updated = False
-
-        # Hide all the things!
-        self.parent.game_end_check(screen_updated)
-
-        if self.is_running:
-            if not self.parent.game_end_fail and not self.parent.game_end_keys_entered:
-                sleep(2)
-                if not self.parent.timed_out:
-                    self.parent.update_label(self.parent.message_label, self.parent.default_message)
-                    self.parent.update_label(self.parent.code_label, '')
-                else:
-                    self.parent.game_end_timeout()
 
 
 class Spinner():
@@ -128,25 +51,19 @@ class Game(QWidget):
         self.spinner = Spinner()
         self.tries_remaining = 3
         self.game_end_fail = False
-        self.game_end_keys_entered = False
-        self.timed_out = False
+        self.game_end_success = False
         self.default_message = 'Waiting for database key ...'
         self.secret_keys = {'9291184': False,
                             '117193': False,
                             'TEST3': False}
-        self.secret_password = 'SecretPassword'
-        self.background_image_path = self.resource_path("Webster_Notes.png")
+        self.secret_password = ''
+        self.background_image_path = self.resource_path("Webster_notes.png")
         self.font = "Times"
-        self.input_text = None
         self.font_size = 8
-        self.timeout = 20 # in seconds
-        self.wait_loop = None
-        self.threadpool = QThreadPool()
         self.initUI()
         
     def initUI(self):
-        myfont = QFont(self.font, self.font_size)
-        self.threadpool.setMaxThreadCount(1)
+        myfont = QFont(self.font, self.font_size) 
 
         oImage = QImage(self.background_image_path)
         sImage = oImage.scaled(QSize(1500, 800))
@@ -178,7 +95,7 @@ class Game(QWidget):
 
         self.qline_edit.returnPressed.connect(self.qbutton.click)
         self.qbutton.clicked.connect(self.on_qbutton_clicked)
-        self.timer.timeout.connect(self.timer_tick)
+        self.timer.timeout.connect(self.timerTick)
 
         self.qbutton.resize(self.qbutton.sizeHint())
         self.setGeometry(300, 300, 1500, 800)
@@ -194,21 +111,22 @@ class Game(QWidget):
 
         return os.path.join(base_path, relative_path)
 
-    def update_timer_display(self):
+    def updateTimerDisplay(self):
         time = "%d:%02d" % (self.time_left/60, self.time_left % 60)
         self.update_label(self.timer_label, time)
 
-    def start_timer(self):
-        self.time_left = self.timeout
-        self.update_timer_display()
+    def startTimer(self):
+        self.time_left = 10
+        self.updateTimerDisplay()
         self.timer.start(1000)
 
-    def timer_tick(self):
+    def timerTick(self):
         self.time_left -= 1
-        self.update_timer_display()
+        self.updateTimerDisplay()
         if self.time_left <= 0:
            self.timer.stop()
-           self.game_end_timeout()
+           self.game_end_failure()
+           
 
     def tries_left(self):
         self.tries_remaining = self.tries_remaining - 1
@@ -229,58 +147,92 @@ class Game(QWidget):
         else:
             return message.format(self.tries_left(), 'tries')
 
-    def stop_thread(self):
-        self.wait_loop.stop()
+    def check_test_sleep(self, text):
+        for i in self.spinner.calculate_rotations():
+            self.update_label(self.message_label, self.check_message(text))
+            sleep(self.spinner.speed)
+        self.update_label(self.message_label, 'Done')
+        self.spinner.reset()
+        sleep(0.5)
 
-    def game_end_timeout(self):
-        if self.wait_loop is not None:
-            if not self.wait_loop.is_running:
-                message_text = 'Database shutdown, system intrusion detected'
-                self.update_label(self.message_label, message_text)
-        self.qline_edit.hide()
-        self.code_label.hide()
-        self.success_message_label.hide()
-        self.qbutton.hide()
-        self.timer.stop()
-        self.timer_label.hide()
-        self.game_end_fail = True
-        self.timed_out = True
-        if self.wait_loop is not None:
-            self.stop_thread()
+    def check_text(self, text):
+        self.check_test_sleep(text)   
+        code_text = None
+        success_message_text = ''
+        num_guessed = 0
 
-    def game_end_failure(self, screen_updated):
-        if not screen_updated:
+        for key, value in self.secret_keys.items():
+            if text.upper() == key:
+                message_text = 'Key entered'
+                code_text = 'Key accepted'
+                self.secret_keys[key] = True
+                break
+
+        if code_text is None:
+            message_text = self.not_found_message()
+            code_text = ''
+
+        for key, value in self.secret_keys.items():
+            if value:
+                num_guessed += 1
+
+        if num_guessed == len(self.secret_keys.keys()):
+            message_text = 'Top Secret access granted. Welcome Agent 69-88'
+            success_message_text = 'Document Password:'
+                                
+            code_text = self.secret_password
+            self.game_end_success = True
+
+        # Game over
+        if self.tries_remaining == 0 and code_text == '':
             message_text = 'Database shutdown, system intrusion detected'
-            self.update_label(self.message_label, message_text)
+            self.game_end_fail = True
+
+        return message_text, code_text, success_message_text
+
+    def game_end_failure(self):
         self.qline_edit.hide()
         self.code_label.hide()
         self.success_message_label.hide()
         self.qbutton.hide()
         self.timer.stop()
         self.timer_label.hide()
-        self.game_end_fail = True
 
     def game_end_success(self):
         self.qline_edit.hide()
         self.qbutton.hide()
         self.timer.stop()
-        self.game_end_keys_entered = True
 
-    def game_end_check(self, screen_updated):
+    def game_end_check(self):
         if self.game_end_fail:
-            self.game_end_failure(screen_updated)
-        if self.game_end_keys_entered:
+            self.game_end_failure()
+        if self.game_end_success:
             self.game_end_success()
 
     @pyqtSlot()
     def on_qbutton_clicked(self):
-        self.wait_loop = WaitLoop(parent=self)
-        self.threadpool.start(self.wait_loop)
+        input_text = self.qline_edit.text()
+        self.qline_edit.clear()
+        self.update_label(self.code_label, '')
+        message_text, code_text, success_message_text = self.check_text(input_text)
+
+        # When we click the button make sure we update the messages after processing
+        self.update_label(self.message_label, message_text)
+        self.update_label(self.success_message_label, success_message_text)
+        self.update_label(self.code_label, code_text)
+
+        # Hide all the things!
+        self.game_end_check()
+
+        if not self.game_end_fail and not self.game_end_success:
+            sleep(2)
+            self.update_label(self.message_label, self.default_message)
+            self.update_label(self.code_label, '')
 
 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
     game = Game()
-    game.start_timer()
+    game.startTimer()
     sys.exit(app.exec_())
